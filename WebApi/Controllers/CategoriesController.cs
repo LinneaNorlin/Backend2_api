2f51b4818068734b05bc4ab6d143c3bcf235a75f
Lisono <linnor74@gmail.com>
2022-11-19 11:55:00 +0100

    Category controller added

---
+++ WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var Product = await _context.Products.FindAsync(id);
+            if (Product != null)
+                return new OkObjectResult(new CategoryResponse
+                {
+                    Category = Product.Category,
+                    
+                    ProductName = Product.ProductName,
+                    ArticleNumber = Product.ArticleNumber
+
+                });
+
+            return new NotFoundResult();
+        }
+
+    }
+}
---
+++ WebApi/Models/CategoryResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class CategoryResponse
+    {
+        public CategoryRequest Category { get; set; } = null!;
+
+        public string ProductName { get; set; } = null!;
+
+        public string ArticleNumber { get; set; } = null!;
+
+    }
+}
